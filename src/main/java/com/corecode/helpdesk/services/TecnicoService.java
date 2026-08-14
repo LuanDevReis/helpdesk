21a6f484149476c83ccf45992e58cfd72b21a779
Luan Reis <luannascimento.microcamp@gmail.com>
2026-08-14 16:19:55 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/corecode/helpdesk/services/TecnicoService.java
+++ src/main/java/com/corecode/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import com.corecode.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id:" + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
