ca6c93983ba9ff2389c2d9f6cfba8f3476e0c139
Luan Reis <luannascimento.microcamp@gmail.com>
2026-08-14 14:53:31 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/corecode/helpdesk/services/TecnicoService.java
+++ src/main/java/com/corecode/helpdesk/services/TecnicoService.java
@@ -3,6 +3,7 @@ package com.corecode.helpdesk.services;
 
 import com.corecode.helpdesk.domain.Tecnico;
 import com.corecode.helpdesk.repositories.TecnicoRepository;
+import com.corecode.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id:" + id));
     }
 }
