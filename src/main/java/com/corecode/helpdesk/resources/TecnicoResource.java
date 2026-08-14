d921cdd180781931e41c8918873394315a47614a
Luan Reis <luannascimento.microcamp@gmail.com>
2026-08-14 12:46:23 -0300

    configuração para receber DTO

--- src/main/java/com/corecode/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/corecode/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.corecode.helpdesk.resources;
 
 import com.corecode.helpdesk.domain.Tecnico;
+import com.corecode.helpdesk.domain.dtos.TecnicoDTO;
 import com.corecode.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +21,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
 
     }
 }
