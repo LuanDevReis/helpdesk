88a89e3acb83865bbfe84cbcf5598f3c747d212e
Luan Reis <luannascimento.microcamp@gmail.com>
2026-08-20 14:48:51 -0300

    delete para tecnico implementado

--- src/main/java/com/corecode/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/corecode/helpdesk/resources/TecnicoResource.java
@@ -54,4 +54,10 @@ public class TecnicoResource {
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
 
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
