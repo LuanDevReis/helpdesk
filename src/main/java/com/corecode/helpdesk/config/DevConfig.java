c17242ae47c274dee29561a829cf02ab4feab12e
Luan Reis <luannascimento.microcamp@gmail.com>
2026-09-17 14:45:38 -0300

    test: configure test profile with H2 database

--- src/main/java/com/corecode/helpdesk/config/DevConfig.java
+++ src/main/java/com/corecode/helpdesk/config/DevConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("test")
 public class DevConfig {
 
     @Autowired
