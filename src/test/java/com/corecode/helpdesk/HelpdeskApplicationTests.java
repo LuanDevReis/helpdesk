49b8a062fc8dc6c46384f1c5ef0d63823a6c897c
Luan Reis <luannascimento.microcamp@gmail.com>
2026-09-23 16:58:45 -0300

    profile test active of CI

--- src/test/java/com/corecode/helpdesk/HelpdeskApplicationTests.java
+++ src/test/java/com/corecode/helpdesk/HelpdeskApplicationTests.java
@@ -2,8 +2,10 @@ package com.corecode.helpdesk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HelpdeskApplicationTests {
 
 	@Test
